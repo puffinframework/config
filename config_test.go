79a6c199d3a63f4bcfe8a23e3ebebd764604ff2b
dfreire <dario.freire@gmail.com>
2014-12-07 14:04:09 +0000

    update

--- config_test.go
+++ config_test.go
@@ -3,9 +3,12 @@ package config_test
 import (
 	"github.com/puffinframework/config"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
 func TestMode(t *testing.T) {
 	assert.Equal(t, "development", config.GetMode())
+	os.Setenv("PF_MODE", "test")
+	assert.Equal(t, "test", config.GetMode())
 }
