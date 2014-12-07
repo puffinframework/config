86ce7b4023d6bddcfcb3cf8cc52e5cfc66c28df1
dfreire <dario.freire@gmail.com>
2014-12-07 17:47:50 +0000

    update

--- config.go
+++ config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"fmt"
+	"log"
+	"github.com/BurntSushi/toml"
 	"os"
 	"strings"
 )
@@ -15,13 +17,24 @@ const (
 
 var (
 	ErrEnvVarNotSet error = fmt.Errorf("config: %s environment variable is not set", ENV_VAR_NAME)
+	ErrReadConfig   error = fmt.Errorf("config: couldn't read config file")
 )
 
 func MustGetMode() string {
 	mode := strings.ToLower(os.Getenv(ENV_VAR_NAME))
 	switch mode {
 	case MODE_PRODUCTION, MODE_DEVELOPMENT, MODE_TEST:
-		return mode
+		// ok
+	default:
+		log.Panic(ErrEnvVarNotSet)
 	}
-	panic(ErrEnvVarNotSet)
+	return mode
+}
+
+func MustReadConfig(config interface{}) {
+	mode := MustGetMode()
+	file := strings.Join([]string{mode, "toml"}, ".")
+	if _, err := toml.DecodeFile(file, config); err != nil {
+		log.Panic(ErrReadConfig)
+	}
 }
--- config_test.go
+++ config_test.go
@@ -19,3 +19,42 @@ func TestMode(t *testing.T) {
 	os.Setenv("PF_MODE", "devELOPmenT")
 	assert.Equal(t, config.MODE_DEVELOPMENT, config.MustGetMode())
 }
+
+func TestReadConfig(t *testing.T) {
+	os.Setenv("PF_MODE", "test")
+
+	type A struct {
+		A1 string
+		A2 string
+	}
+
+	type B struct {
+		Bs []string
+	}
+
+	type C struct {
+		D struct {
+			Cd1 string
+		}
+	}
+
+	type configAB struct {
+		A A
+		B B
+	}
+
+	cfg1 := &configAB{}
+	config.MustReadConfig(cfg1)
+	assert.Equal(t, "a1", cfg1.A.A1)
+
+	type configAC struct {
+		A A
+		C C
+	}
+
+	cfg2 := &configAC{}
+	config.MustReadConfig(cfg2)
+	assert.Equal(t, "a1", cfg2.A.A1)
+
+
+}
